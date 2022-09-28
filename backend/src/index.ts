79c43645dc875865bdfdb57e518db6333529f1e4
Hasan Cutcu <hasancutcu@windowslive.com>
2022-09-28 15:30:52 +0100

    json file added, stream reader added

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -1,1 +1,9 @@
-console.log('Hello there!');
+import fs from 'fs';
+
+const stream = fs.createReadStream('./db/db.json', {highWaterMark: 150 * 1024,  encoding: 'utf8' });
+
+stream.on('data', (chunk) => {
+  console.log(chunk);
+  console.log("***************************");
+  
+});
