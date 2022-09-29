1db17a5c7e9fdaf8043f94667c3662b051304b47
Hasan Cutcu <hasancutcu@windowslive.com>
2022-09-29 14:50:47 +0100

    frontend boilerplate

---
+++ frontend/src/app/layout/App.js
@@ -0,0 +1,15 @@
+import Footer from '../feature/footer/Footer';
+import Header from '../feature/header/Header';
+import Hero from '../feature/hero/Hero';
+
+function App() {
+  return (
+    <>
+      <Header />
+      <Hero />
+      <Footer />
+    </>
+  );
+}
+
+export default App;
